0361845b80b4c72e12d76b58733d397215c5e467
Lori Rothermel <lori@rothermel1.com>
2025-01-02 19:44:51 -0800

    Initial Commit

---
+++ BBQuotes17/BBQuotes17App.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotes17App.swift
+//  BBQuotes17
+//
+//  Created by Lori Rothermel on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotes17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
